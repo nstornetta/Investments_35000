29a4dd00d27919d23c56a9a254538be57f404e4a
Nathan Stornetta <nathaniel.stornetta@gmail.com>
2018-04-07 01:01:10 -0500

    clean up perpetuity node class and improve DRY

--- timeline.py
+++ timeline.py
@@ -7,23 +7,25 @@ class TimelineNode(object):
         self.time_period = time_period
         self.timeline_graphic = "---|---"
 
+    def __repr__(self):
+        return "{}\nCash: {} Time Period: {}".format(self.__class__.__name__,
+                                                     self.cash,
+                                                     self.time_period)
+
 
 class PerpetuityNode(TimelineNode):
     def __init__(self, cash, time_period, growth_rate=0):
-        self.cash = cash
-        self.time_period = time_period  # indicates time period of first payment
+        super().__init__(cash, time_period)
         self.growth_rate = growth_rate  # only relevant if growth rate > 0
-        self.timeline_graphic = "--- Perpetuity ---"
+        self.timeline_graphic = "-- Perpetuity --"
 
 
 class Timeline(object):
-    def __init__(self, discount_rate, compound_rate=1, nodes=None, perpetuity=None):
+    def __init__(self, discount_rate, compound_rate=1, nodes=None):
         self.compound_rate = compound_rate
         self.discount_rate = discount_rate
         self.pv = None
-
-        # TODO: remove implicit assumption that perpetuity occur at the end of any timeline
-        self.perpetuity = None  # initialize to None to avoid issue with update_pv logic TODO: make this cleaner
+        self.terminal_value = None
 
         if nodes is None:
             self.nodes = []
@@ -33,67 +35,52 @@ class Timeline(object):
         else:
             raise TypeError("nodes has to be either None or a list of TimelineNodes")
 
-        if perpetuity and isinstance(perpetuity, PerpetuityNode):
-            self.perpetuity = perpetuity
-            self.update_pv()
-        elif perpetuity:
-            raise TypeError("perpetuity has to be either None or a PerpetuityNode")
-
-    def __getitem__(self, item):
-        pass
+    def __getitem__(self, key):
+        return self.nodes.__getitem__(key)
 
     def __repr__(self):
         """Print out a set of lines with notches for each payment"""
         return_string = "discount rate: {}\n".format(round(self.discount_rate, 2))
         return_string += "PV: {}\n".format(round(self.pv, 2))
 
-        node_graphic = "---|---"
-        perpetuity_graphic = "--- Perpetuity ---"
-        return_string += node_graphic*len(self.nodes)
-        if self.perpetuity:
-            return_string += perpetuity_graphic
+        # Add in the basic timeline graphics
+        for node in self.nodes:
+            return_string += node.timeline_graphic
         return_string += "\n"
 
+        # Add indication of time period for each node
         for node in self.nodes:
-            # Assume that time period <=99
-            return_string += str(node.time_period).center(len(node_graphic))
-        if self.perpetuity:
-            return_string += str(self.perpetuity.time_period).center(len(perpetuity_graphic))
+            return_string += str(node.time_period).center(len(node.timeline_graphic))
         return_string += "\n"
 
+        # Add cash for each node
         for node in self.nodes:
-            return_string += "C:{cash}".format(cash=node.cash).center(len(node_graphic))
-        if self.perpetuity:
-            return_string += "C:{cash}".format(cash=self.perpetuity.cash).center(len(perpetuity_graphic))
+            return_string += "C:{cash}".format(cash=node.cash).center(len(node.timeline_graphic))
 
         return return_string
 
     def add_node(self, node):
+        if not isinstance(node, TimelineNode):
+            raise TypeError("node has to be a timelineNode object")
         self.nodes.append(node)
         self.nodes = sorted(self.nodes, key=lambda n: n.time_period)
         self.update_pv()
 
-    def add_perpetuity(self, cash, time_period, growth_rate=0):
-        self.perpetuity = PerpetuityNode(cash, time_period, growth_rate)
-        self.update_pv()
-
     def update_pv(self):
         self.pv = 0
+        if self.terminal_value is not None:
+            self.terminal_value = 0
         for node in self.nodes:
-            self.pv += self.pv_node(node)
-        if self.perpetuity:
-            self.pv += self.pv_perpetuity()
+            if isinstance(node, PerpetuityNode):
+                self.pv += self.pv_perpetuity(node)
+            else:
+                self.pv += self.pv_node(node)
 
     def pv_node(self, node):
-        return round(node.cash / (1+self.discount_rate) ** node.time_period, 2)
+        return round(node.cash / (1 + self.discount_rate) ** node.time_period, 2)
 
-    def pv_perpetuity(self, perpetuity_node=None):
-        if not perpetuity_node:
-            perpetuity_node = self.perpetuity
-        else:
-            perpetuity_node = perpetuity_node
+    def pv_perpetuity(self, perpetuity_node):
         # first, calculate pv in terms of time period before perpetuity per std. equation
         pv = perpetuity_node.cash / (self.discount_rate - perpetuity_node.growth_rate)
-        # second, discount pv back to terms of time period 0
-        pv = pv / (1 + self.discount_rate) ** (perpetuity_node.time_period-1)
-        return pv
+        # second, discount pv back to being in terms of time period 0
+        return pv / (1 + self.discount_rate) ** (perpetuity_node.time_period-1)
